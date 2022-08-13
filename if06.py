f41555fe679346d06e710146d7829f48db51437f
Samandar000 <samandarnfh.2020@gmail.com>
2022-08-13 13:56:27 +0500

    06

--- if06.py
+++ if06.py
@@ -13,4 +13,23 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
-    return
+    if a > 0:
+        x1=1
+    else:
+        x1=0
+    if b > 0:
+        x2=1
+
+    else:
+        x2=0
+    if c > 0:
+        x3=1
+    else:
+        x3=0
+        
+    if x1+x2+x3 > 1:
+        return 'there are a lot of positive numbers'
+    else:
+        return 'there are a lot of negative numbers'
+
+print(main(-2,-5,2))
