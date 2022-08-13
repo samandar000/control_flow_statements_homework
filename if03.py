5ed54f65a8fbd462d13e9a3268a3c768272f2bfb
Samandar000 <samandarnfh.2020@gmail.com>
2022-08-13 12:14:29 +0500

    03

--- if03.py
+++ if03.py
@@ -6,4 +6,11 @@ def main(a):
     Returns:
         a: integer
     """
-    return
+    if a == 0:
+        return 10
+    if a > 0:
+        return a+1
+    else:
+        return a-2
+
+print(main(0))
